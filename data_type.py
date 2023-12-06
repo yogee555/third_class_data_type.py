3c4842297043085cdd2133771b4e30f795c2e851
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2023-12-06 11:22:19 +0530

    Add files via upload

---
+++ data_type.py
@@ -0,0 +1,35 @@
+# PYTHON DATA TYPES :
+# numeric
+numeric = 8
+print(type(numeric))
+
+# float
+float_val = 8.9
+print(type(float_val))
+
+# complex_val
+complex_val = 2+3j
+print(type(complex_val))
+
+# string
+str_val = 'hi welcome to the class'
+str_val_val = 'welcome'
+
+print(type(str_val))
+print(type(str_val_val))
+
+# dictionary
+dict_val = {"vani":999,"akshay":666,"harshith":555}
+print(type(dict_val))
+print(dict_val["vani"])
+
+# list
+list_val = [1,2,3,4,5]
+print(type(list_val))
+print(list_val[2])
+
+# tuple
+tuple_val = (1,2,3,4,5)
+print(type(tuple_val))
+print(tuple_val[0])
+
